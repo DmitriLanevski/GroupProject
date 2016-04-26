fe842a737b5cdcb7275190a62d99ad09c3dc0b1a
Cerapa <madisjanno@gmail.com>
2016-04-26 21:48:09 +0300

    Removed weird duplicate Buffs/Skills folder.

--- src/main/java/gameLogic/Buffs/Skills/Skill.java
+++
@@ -1,8 +0,0 @@
-package gameLogic.buffs.Skills;
-
-import gameLogic.Character;
-
-public class Skill {
-    Character selfTarget;
-    Character oponentTarget;
-}
--- src/main/java/gameLogic/Buffs/Skills/SkillCollection.java
+++
@@ -1,7 +0,0 @@
-package gameLogic.buffs.Skills;
-
-/**
- * Created by lanev_000 on 16.04.2016.
- */
-public class SkillCollection {
-}
