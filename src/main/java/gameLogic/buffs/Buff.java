0b420b131db6f961b2103b6bb3d67924ec1c6b6e
Cerapa <madisjanno@gmail.com>
2016-04-26 22:11:30 +0300

    Added some documentation for the Buff class.

--- src/main/java/gameLogic/buffs/Buff.java
+++ src/main/java/gameLogic/buffs/Buff.java
@@ -12,30 +12,60 @@ public abstract class Buff {
         this.opponent = opponent;
     }
 
+    /**
+     *  Called on initial application of buff.
+     */
     public void onApplied() {
 
     }
+
+    /**
+     *  Called right before the buff is removed from the list of active buffs.
+     */
     public void onRemoved() {
 
     }
+
+    /**
+     * @return A boolean indicating if the buff should be removed from the list of active buffs.
+     */
     public boolean isExpired() {
         return true;
     }
 
+    /**
+     * Called when buffed character uses an attack skill.
+     */
     public void onAttack() {
 
     }
+
+    /**
+     * Called when the opponent uses an attack skill.
+     */
     public void onDefend() {
 
     }
 
+    /**
+     * Called when buffed character takes damage.
+     * @param amount The amount of damage taken.
+     */
     public void onDamageTaken(double amount) {
     }
 
+    /**
+     * Called when the value of a stat is changed.
+     * @param statusName Name of changed stat e.g.: "Mana" or "Stamina"
+     * @param amount The amount of change.
+     */
     public void onStatusChange(String statusName, double amount) {
 
     }
 
+    /**
+     * Called each time tick.
+     */
     public void onTick(){
     }
 
