4556453e57f79e4fa08c6d9f0d6dfc0e0670b406
Cerapa <madisjanno@gmail.com>
2016-05-14 18:57:17 +0300

    Skills now have descriptions

---
+++ src/main/java/client/ui/CharacterCreateScreen.java
@@ -0,0 +1,19 @@
+package client.ui;
+
+import server.Message;
+
+/**
+ * Created by Madis on 14.05.2016.
+ */
+public class CharacterCreateScreen extends UIManager {
+    public CharacterCreateScreen(UIManager parentManager) {
+        super(parentManager);
+    }
+
+    @Override
+    public void handleMessage(Message message) {
+        super.handleMessage(message);
+
+
+    }
+}
